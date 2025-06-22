0d11e0c713b880988c0ad5cf8bfefa1dae334ab4
Rexter Dayuta <rexterdayuta@gmail.com>
2025-06-23 01:15:36 +0800

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -28,40 +28,38 @@ class Home extends BaseController
         ];
 
         $user_id = $_SESSION['user_id'];
-        $db = $this->default_db;
-        $sharedDbName = 'abstract_suit_shared_db'; // Use the shared database name directly
 
         // Fetch papers
         $papersQuery = "
             SELECT p.*, p.id AS id, pt.type AS paper_type, u.name AS user_name
             FROM papers p
             LEFT JOIN paper_type pt ON p.type_id = pt.id
-            LEFT JOIN {$sharedDbName}.users u ON p.user_id = u.id
+            LEFT JOIN {$this->shared_db_name }.users u ON p.user_id = u.id
             WHERE p.user_id = ?
             ORDER BY p.id ASC
         ";
-        $papers = $db->query($papersQuery, [$user_id])->getResult();
+        $papers = $this->default_db->query($papersQuery, [$user_id])->getResult();
 
         foreach ($papers as $paper) {
             // Fetch authors for the paper
             $authorsQuery = "
                 SELECT u.*, up.signature_signed_date
                 FROM paper_authors pa
-                LEFT JOIN {$sharedDbName}.users u ON pa.author_id = u.id
-                LEFT JOIN {$sharedDbName}.users_profile up ON pa.author_id = up.author_id
+                LEFT JOIN {$this->shared_db_name }.users u ON pa.author_id = u.id
+                LEFT JOIN {$this->shared_db_name }.users_profile up ON pa.author_id = up.author_id
                 WHERE pa.paper_id = ? AND pa.author_type = 'author'
                 AND pa.id NOT IN (
                     SELECT paper_author_id FROM removed_paper_authors
                 )
             ";
-            $paper->authors = $db->query($authorsQuery, [$paper->id])->getResultArray();
+            $paper->authors = $this->default_db->query($authorsQuery, [$paper->id])->getResultArray();
 
             // Fetch reviewers for the paper
             $reviewersQuery = "
                 SELECT * FROM abstract_review
                 WHERE abstract_id = ?
             ";
-            $paper->reviewers = $db->query($reviewersQuery, [$paper->id])->getResult();
+            $paper->reviewers = $this->default_db->query($reviewersQuery, [$paper->id])->getResult();
 
             // Fetch uploads for each reviewer
             foreach ($paper->reviewers as &$reviewer) {
@@ -70,17 +68,17 @@ class Home extends BaseController
                     WHERE paper_id = ? AND reviewer_id = ?
                     LIMIT 1
                 ";
-                $reviewer->uploads = $db->query($uploadsQuery, [$paper->id, $reviewer->reviewer_id])->getRow();
+                $reviewer->uploads = $this->default_db->query($uploadsQuery, [$paper->id, $reviewer->reviewer_id])->getRow();
             }
 
             // Fetch panelists for the paper
             $panelistsQuery = "
                 SELECT u.*, pa.is_copyright_agreement_accepted
                 FROM paper_authors pa
-                LEFT JOIN {$sharedDbName}.users u ON pa.author_id = u.id
+                LEFT JOIN {$this->shared_db_name }.users u ON pa.author_id = u.id
                 WHERE pa.paper_id = ? AND pa.author_type = 'panelist'
             ";
-            $paper->panelist = $db->query($panelistsQuery, [$paper->id])->getResult();
+            $paper->panelist = $this->default_db->query($panelistsQuery, [$paper->id])->getResult();
         }
 
         $data['papers'] = $papers;
