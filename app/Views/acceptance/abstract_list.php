047103027b6fa3b8b6bf56ed775db2ce80c11897
Rexter Dayuta <rexterdayuta@gmail.com>
2025-12-03 12:22:17 +0800

    update : field change to agreement

--- app/Views/acceptance/abstract_list.php
+++ app/Views/acceptance/abstract_list.php
@@ -255,7 +255,7 @@
                                 January 15, 2026
                             </td>
                             <td>
-                                ${(val.author_acceptance_data && val.author_acceptance_data.impact_statement.trim() !== '' ) ? '<span class="badge bg-success text-white" >Complete </span>' : '<span class="badge bg-danger text-white" >Incomplete </span>'}
+                                ${(val.author_acceptance_data && val.author_acceptance_data.impact_statement_agreement) ? '<span class="badge bg-success text-white" >Complete </span>' : '<span class="badge bg-danger text-white" >Incomplete </span>'}
                             </td>
                             <td>
                                  ${openImpactStatement}
