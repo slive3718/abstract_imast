12e2a649239bfcce74b92fd75ccd91a3e958b305
Rexter Dayuta <rexterdayuta@gmail.com>
2025-12-16 07:44:31 +0800

    Update UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -21,7 +21,7 @@ class UserModel extends BaseModel
     protected bool $updateOnlyChanged = true;
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
