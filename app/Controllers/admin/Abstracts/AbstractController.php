94f58bc8ad909a5320a81088757a42b7ba028cc3
Rexter Dayuta <rexterdayuta@gmail.com>
2026-05-04 23:06:26 +0800

    search not include deleted users

--- app/Controllers/admin/Abstracts/AbstractController.php
+++ app/Controllers/admin/Abstracts/AbstractController.php
@@ -1110,6 +1110,7 @@ class AbstractController extends BaseController
             ->orLike('email', $searchValue)
             ->orLike('surname', $searchValue)
             ->groupEnd()
+                ->where('deleted_at', null)
             ->findAll();
 
         return $this->response->setJSON($results);
