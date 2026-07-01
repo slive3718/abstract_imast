aacce5e6b31f2baa420f8449830d6c4c6a63878d
Rexter Dayuta <rexterdayuta@gmail.com>
2026-07-01 13:51:26 +0800

    Update User.php

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -511,7 +511,7 @@ class User extends BaseController
                 continue;
             $authorDisclosure = $disclosure[$author['author_id']]['disclosure'] ?? [];
             $author['mailLogs'] = $mailLogs;
-            $author['disclosure'] = $authorDisclosure;
+            $author['disclosure'] = $AppDisclosureService->getAuthorsByIdWithDisclosures($author['author_id'])?? [];
             $author['disclosureStatus'] =  $AppDisclosureService->getStatus($author['author_id'])?? [];;
             $author['signature_signed_date'] = $authorDisclosure['updated_at'] ??  $authorDisclosure['created_at'] ?? '';
             $paperAuthorsArray[] = $author;
