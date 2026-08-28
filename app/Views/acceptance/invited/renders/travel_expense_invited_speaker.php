7c8b3c660ad8fdc892017bcc95f52447fe4acbae
Rexter Dayuta <rexterdayuta@gmail.com>
2026-08-28 13:29:02 +0800

    Create travel_expense_invited_speaker.php

---
+++ app/Views/acceptance/invited/renders/travel_expense_invited_speaker.php
@@ -0,0 +1,13 @@
+<div>
+    <p>
+        Upon acceptance as an invited speaker to IMAST, the Scoliosis Research Society will provide you with:
+    </p>
+    <ul>
+        <li>Travel & Hotel Expense: Please refer to your invitation</li>
+        <li>Meeting Registration: <a href="https://www.srs.org/Meetings-Conferences/IMAST/IMAST2027" target="_blank">Complimentary IMAST Meeting Registration</a></li>
+    </ul>
+    <p>All other travel expenses will be the responsibility of the speaker.</p>
+    <p><span class="text-danger">*</span><i>Please note: SRS will register invited speakers for the meeting. There is no need to register yourself.</i></p>
+    <p>For additional information regarding the program and travel details, please <a href="https://www.srs.org/Meetings-Conferences/IMAST/IMAST2027" target="_blank">visit www.srs.org/imast</a>.</p>
+    <p>If you have any questions, please email <a href="mailto:education@srs.org" target="_blank">education@srs.org</a></p>
+</div>
